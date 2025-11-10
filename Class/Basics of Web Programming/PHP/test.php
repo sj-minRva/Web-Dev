630efbd5f22bd78b9ec329e9ef162fd479151c7b
sj-minRva <rheamaria13@gmail.com>
2025-11-10 13:48:38 +0530

    php practice

--- Class/Basics of Web Programming/PHP/test.php
+++ Class/Basics of Web Programming/PHP/test.php
@@ -1,3 +1,25 @@
 <?php
-echo "hello world";
+class Car{
+    public $color;
+    public $model;
+
+    public function __construct($color = null, $model = null){
+        $this->color = $color;
+        $this->model = $model;
+    }
+
+    public function startEngine(){
+        echo "The car is a " . $this->color . " " . $this->model . ".\n";
+    }
+}
+
+
+$myCar2 = new Car();
+$myCar2->color = "red";
+$myCar2->model = "Toyota";
+$myCar2->startEngine();
+
+$myCar3 = new Car("blue", "Honda");
+$myCar3->startEngine();
+
 ?>
